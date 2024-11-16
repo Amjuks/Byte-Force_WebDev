703db8f95f0d5c3c6f66c8a8555beaf2815c73a4
Kasvi-1603 <gupta.kasvi@gmail.com>
2024-11-16 15:48:23 +0530

    updates_sc3

--- byte_force/voyage_mate/admin.py
+++ byte_force/voyage_mate/admin.py
@@ -8,6 +8,7 @@ from .models import Itinerary
 from .models import TagPhrase
 from .models import Notification
 from .models import Message
+from .models import City
 
 
 admin.site.register(Destination) 
@@ -16,6 +17,9 @@ admin.site.register(Itinerary)
 admin.site.register(TagPhrase)
 admin.site.register(Notification)
 admin.site.register(Message)
+admin.site.register(City)
+
+
 
 
 
