6e4e823465eb32b0a15cd2d37169e25cb9092478
Kasvi-1603 <gupta.kasvi@gmail.com>
2024-11-16 14:31:37 +0530

    Updated code

--- byte_force/voyage_mate/admin.py
+++ byte_force/voyage_mate/admin.py
@@ -3,10 +3,18 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Destination
+from .models import Review
+from .models import Itinerary
+from .models import TagPhrase
+from .models import Notification
 
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('user', 'destination', 'rating', 'created_at') 
-    search_fields = ('user__username', 'destination__name')  # Enable search by user's username and destination name
-    list_filter = ('rating', 'destination')  # Enable filtering by rating and destination
-    ordering = ('-created_at',)  # Default ordering by created_at in descending order
-    raw_id_fields = ('destination',)  # Use raw ID field for the destination 
+admin.site.register(Destination) 
+admin.site.register(Review)
+admin.site.register(Itinerary)
+admin.site.register(TagPhrase)
+admin.site.register(Notification)
+
+
+
+
+
