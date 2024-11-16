86d4dc8f07fdf63cc06d99a35991e87801624df2
Amjuks <aman.jukaku@gmail.com>
2024-11-16 15:58:35 +0530

    update frontend

--- byte_force/voyage_mate/urls.py
+++ byte_force/voyage_mate/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.IndexView.as_view(), name='index'),
-    path('register/', views.IndexView.as_view(), name='index'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     
     path('', views.IndexView.as_view(), name='index'),
     path('phrase/<str:country>', views.TagPhraseAPIView.as_view(), name='phrase')
--- byte_force/voyage_mate/views.py
+++ byte_force/voyage_mate/views.py
@@ -1,22 +1,68 @@
 import json
 
-from django.http import JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.http import JsonResponse, HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.urls import reverse
 
 from .models import TagPhrase
 
 # Create your views here.
 class IndexView(View):
     def get(self, request):
-
-
-        print(TagPhrase.objects.get(country="IN"))
         return render(request, 'voyage_mate/index.html')
     
-
 class TagPhraseAPIView(View):
     def get(self, request, country: str):
-        
         phrase = TagPhrase.objects.get(country=country).phrase
         return JsonResponse({'phrase': phrase})
+    
+class LoginView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'storyapp/login.html')
+    
+    def post(self, request: HttpRequest) -> HttpResponse:
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("storyapp:dashboard"))
+        else:
+            return render(request, "storyapp/sign-in.html", {
+                "message": "Invalid username and/or password."
+            })
+
+class RegisterView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, 'voyage_mate/register.html')
+    
+    def post(self, request: HttpRequest) -> HttpResponse:
+        username = request.POST["username"]
+
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "voyage_mate/register.html", {
+                "message": "Passwords must match."
+            })
+
+        try:
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "voyage_mate/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+
+class LogoutView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        logout(request)
+        return HttpResponseRedirect(reverse("index"))
