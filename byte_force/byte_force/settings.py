f4219de74d531b76c3984b474c3277131aae063c
Kasvi-1603 <gupta.kasvi@gmail.com>
2024-11-17 00:10:06 +0530

    updates_sc11

--- byte_force/byte_force/settings.py
+++ byte_force/byte_force/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'byte_force.wsgi.application'
-ASGI_APPLICATION = " byte_force.routing.application"
+ASGI_APPLICATION = 'byte_force.routing.application'
 
 
 # Database
